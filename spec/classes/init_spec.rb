0cd33a8f66a0bdea6549742dc0aacb20886a8bd2
bob walker <bob@randomness.org.uk>
2013-09-14 13:57:45 +0100

    Alter tests to fix our current manifests
    Also remove mutli os checks.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,31 +1,12 @@
 require 'spec_helper'
 
 describe 'clamav' do
-  context 'supported operating systems' do
-    ['Debian', 'RedHat'].each do |osfamily|
-      describe "clamav class without any parameters on #{osfamily}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :osfamily => osfamily,
-        }}
+context 'classes' do
+        it { should include_class('clamav::run_freshclam') }
 
-        it { should include_class('clamav::params') }
-
-        it { should contain_class('clamav::install') }
+        it { should contain_class('clamav::package') }
         it { should contain_class('clamav::config') }
         it { should contain_class('clamav::service') }
-      end
     end
-  end
 
-  context 'unsupported operating system' do
-    describe 'clamav class without any parameters on Solaris/Nexenta' do
-      let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
-      }}
-
-      it { expect { should }.to raise_error(Puppet::Error, /Nexenta not supported/) }
-    end
-  end
 end
