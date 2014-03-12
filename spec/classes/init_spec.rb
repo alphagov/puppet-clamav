9b4747c30318e3860373e464ca569701b1581a15
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-03-12 16:20:13 +0000

    Replace `include_class` with `contain_class`
    `include_class` has been deprecated in favour of `contain_class` in rspec. When
    a spec test is started and the spec configuration contains an `include_class`,
    rspec still completes but warns of this deprecation.
    
    This commit fixes the deprecation and means we no longer get warned about it.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'clamav' do
 context 'classes' do
-        it { should include_class('clamav::run_freshclam') }
+        it { should contain_class('clamav::run_freshclam') }
 
         it { should contain_class('clamav::package') }
         it { should contain_class('clamav::config') }
